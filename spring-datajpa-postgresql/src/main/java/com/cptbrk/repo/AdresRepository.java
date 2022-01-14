9520da5d4020523f9f4ad020aea234eb1601e5f3
BurakCalisgan <calisganburak@gmail.com>
2022-01-14 20:38:09 +0300

    Rest api with Postgresql

---
+++ spring-datajpa-postgresql/src/main/java/com/cptbrk/repo/AdresRepository.java
@@ -0,0 +1,8 @@
+package com.cptbrk.repo;
+
+import com.cptbrk.entity.Adres;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdresRepository extends JpaRepository<Adres, Long> {
+
+}
---
+++ spring-datajpa-postgresql/src/main/java/com/cptbrk/repo/KisiRepository.java
@@ -0,0 +1,7 @@
+package com.cptbrk.repo;
+
+import com.cptbrk.entity.Kisi;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KisiRepository extends JpaRepository<Kisi, Long> {
+}
